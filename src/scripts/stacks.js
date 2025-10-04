ce3aa45397e5aa5ae8de1a830e560a4fa7487494
Mateus Ferreira <mateusjunior803@gmail.com>
2025-10-04 00:50:35 -0300

    :sparkles: feat: Complete Skills section
    Finalize the Skills section with all content, icons, and styling.

---
+++ src/scripts/stacks.js
@@ -0,0 +1,26 @@
+async function getStackData() {
+  const PATH = "./src/data/stacks.json";
+
+  try {
+    const response = await fetch(PATH);
+    const stacks = await response.json();
+    return stacks;
+
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function showStackIcons() {
+  const stackContainer = document.querySelector(".stacks");
+  const stacks = await getStackData();
+
+  stacks.forEach(stack => {
+    stackContainer.insertAdjacentHTML("beforeend", `
+        <img src="${stack.iconUrl}" alt="${stack.label}'s icon" class="icon" title="${stack.label}">
+      `
+    )
+  });
+}
+
+showStackIcons()
