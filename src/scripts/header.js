685e4b1bfedf27da3e44847fb95a2b3fc14276eb
Mateus Ferreira <mateusjunior803@gmail.com>
2025-10-04 21:36:22 -0300

    :iphone: feat: Make header responsive with effects and overlay
    Implemented responsive design for the header, including smooth visual
    effects and a dark overlay for better readability and style consistency
    across devices.

---
+++ src/scripts/header.js
@@ -0,0 +1,37 @@
+const hamburguerBtn = document.querySelector(".hamburguer-menu");
+const navLinks = document.querySelectorAll("nav ul li a");
+const overlay = document.querySelector(".overlay");
+const toggleMenu = () => {
+  const body = document.querySelector("body");
+  const nav = document.querySelector("header nav");
+  const navIsActive = nav.classList.toggle("active");
+  const menuIcon = document.querySelector(".lucide-menu");
+  const closeIcon = document.querySelector(".lucide-x");
+
+  closeIcon.classList.toggle("hidden");
+  menuIcon.classList.toggle("hidden");
+  overlay.classList.toggle("hidden");
+  body.classList.toggle("no-scroll");
+  hamburguerBtn.setAttribute("aria-label", navIsActive ? "Close menu" : "Open menu");
+  hamburguerBtn.setAttribute("aria-expanded", String(navIsActive));
+}
+const toggleSticky = () => {
+  const header = document.querySelector("header");
+  const logo = header.querySelector("img");
+  const actDownloadBtn = document.querySelector(".actions button");
+  const navDownloadBtn = document.querySelector("nav ul li button");
+  const isSticky = window.scrollY >= 100;
+
+  logo.setAttribute("src", `./src/assets/logo/logo-mf-${isSticky ? "light" : "dark"}.svg`);
+  header.classList.toggle("sticky", isSticky);
+  actDownloadBtn.classList.toggle("light", isSticky);
+  navDownloadBtn.classList.toggle("light", isSticky);
+  navDownloadBtn.classList.toggle("dark", !isSticky);
+  actDownloadBtn.classList.toggle("dark", !isSticky);
+  overlay.classList.toggle("dark", isSticky);
+}
+
+window.addEventListener("scroll", () => toggleSticky())
+hamburguerBtn.addEventListener("click", () => toggleMenu())
+navLinks.forEach(link => link.addEventListener("click", () => toggleMenu()))
+overlay.addEventListener("click", () => toggleMenu());
