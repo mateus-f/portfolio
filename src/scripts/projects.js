e3e8ce9a194f190049e145b1a2fd0a7d38a9928b
Mateus Ferreira <mateusjunior803@gmail.com>
2025-12-15 23:15:46 -0300

    :sparkles: feat: Add new projects to Projects section

--- src/scripts/projects.js
+++ src/scripts/projects.js
@@ -28,7 +28,7 @@ async function showProjects() {
                   fill="currentColor" />
               </svg>
             </a>
-            <a href="${card.urlPreview}" target="_blank" rel="noopener noreferrer" title="Acesse o site">
+            ${card.urlPreview ? `<a href="${card.urlPreview}" target="_blank" rel="noopener noreferrer" title="Acesse o site">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
                 class="lucide lucide-external-link-icon lucide-external-link">
@@ -36,7 +36,7 @@ async function showProjects() {
                 <path d="M10 14 21 3" />
                 <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" />
               </svg>
-            </a>
+            </a>` : ""}
           </div>
           <img
             src="${card.urlImage}"
